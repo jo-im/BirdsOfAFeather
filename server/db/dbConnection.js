67c7995feff705e073815bb1018385f9bcd94725
Ryan McChesney <rmcchesn@gmail.com>
2016-08-09 13:41:56 -0700

    Set up db connection export

--- server/db/dbConnection.js
+++ server/db/dbConnection.js
@@ -1,20 +1,9 @@
 import Sequelize from 'sequelize';
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+export const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
   dialectOptions: {
     ssl: true
   }
 });
-
-sequelize
-  .authenticate()
-  .then((err) => {
-    console.log('Connection has been established successfully.');
-  }, (err) => {
-    console.log('Unable to connect to the database. Error following *****');
-    console.log(err);
-  });
-
-export default sequelize;
