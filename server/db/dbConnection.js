a90650b6052b27728d640324c3266f15beb6a9e5
Ryan McChesney <rmcchesn@gmail.com>
2016-08-08 19:49:08 -0700

    Successfully authenticate to heroku DB

--- server/db/dbConnection.js
+++ server/db/dbConnection.js
@@ -1,7 +1,20 @@
 import Sequelize from 'sequelize';
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: true
+  }
 });
+
+sequelize
+  .authenticate()
+  .then((err) => {
+    console.log('Connection has been established successfully.');
+  }, (err) => {
+    console.log('Unable to connect to the database. Error following *****');
+    console.log(err);
+  });
 
 export default sequelize;
