a6443b4d124739859fed30975d84bd5aaff6da70
Ryan McChesney <rmcchesn@gmail.com>
2016-08-09 14:04:17 -0700

    Populate concern data.

--- server/db/initialization/initializeConcerns.js
+++ server/db/initialization/initializeConcerns.js
@@ -2,3 +2,18 @@ import {sequelize} from '../dbConnection';
 import Sequelize from 'sequelize';
 import {Concerns} from '../models/concernModel';
 import {concernsList} from './concernData';
+
+console.log(Concerns);
+
+concernsList.forEach((concern) => {
+  Concerns
+    .build({
+      name: concern[0],
+      subConcern: concern[1],
+      majorConcern: concern[2]
+    })
+    .save()
+    .then(() => {
+      console.log('Created Concern', concern[0]);
+    });
+});
