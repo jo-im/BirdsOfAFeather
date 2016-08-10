1a3645b94d47eec524f43ee4dacb64c819229f73
Joann Im <joann_im@hotmail.com>
2016-08-10 14:48:11 -0700

    Create grade and created a .developerprofile

--- public/components/Summary.js
+++ public/components/Summary.js
@@ -3,12 +3,12 @@ import { Text, TouchableHighlight, View, Image } from 'react-native';
 
 export default class Summary extends Component {
   render() {
-    console.log('Inside Summary and this.props.productDescription.image is', this.props.productDescription.image);
   	return (
   	  <View>
   	    <Text>{'\n'}</Text>
         <View style={{flex: 1, flexDirection: 'row'}}>
-          <Image source={{uri:this.props.productDescription.image}} style={{height: 50, width: 50}}></Image>
+          <Image source={{uri:this.props.productDescription.image}} style={{height: 150, width: 150}}></Image>
+          <Text style={{textAlign: 'center', fontSize: 40, fontFamily: 'Didot-Italic'}}>Grade: 86</Text>
         </View>
   	    <Text>User's concerns are {this.props.concerns}</Text>
         <Text>User's allergies are {this.props.allergies}</Text>
