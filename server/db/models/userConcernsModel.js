573946c87b6b1d306d9bf0bdd149762815ff2e58
Ryan McChesney <rmcchesn@gmail.com>
2016-08-09 22:06:25 -0700

    refactor to properly join users and concerns.

--- server/db/models/userConcernsModel.js
+++ server/db/models/userConcernsModel.js
@@ -10,5 +10,6 @@ const User_Concerns = sequelize.define ('User_Concerns', {
 // Create foreign key constraints within join table
 Users.hasMany(Concerns);
 Concerns.belongsToMany(Users, { through: User_Concerns});
+Users.belongsToMany(Concerns, { through: User_Concerns});
 
 export default User_Concerns;
