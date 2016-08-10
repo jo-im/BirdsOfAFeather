0d9c8ae7fe46dd61c9ebd3c425f78a663d56bafb
Ryan McChesney <rmcchesn@gmail.com>
2016-08-09 17:11:48 -0700

    Update user model for unique key requirement on FB id

--- server/db/models/userModel.js
+++ server/db/models/userModel.js
@@ -4,6 +4,6 @@ import Sequelize from 'sequelize';
 // Users model
 export const Users = sequelize.define ('User', {
   name: Sequelize.STRING,
-  facebookId: Sequelize.BIGINT,
+  facebookId: {type: Sequelize.BIGINT, unique: true},
   facebookAccessToken: Sequelize.STRING
 });
