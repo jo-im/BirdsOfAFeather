919c7aa62325e5005591ec458c317e232ddbcb54
Ryan McChesney <rmcchesn@gmail.com>
2016-08-10 10:37:25 -0700

    Implement test for process.env

--- server/db/dbConnection.js
+++ server/db/dbConnection.js
@@ -1,5 +1,5 @@
 import Sequelize from 'sequelize';
-require('dotenv').config();
+if (!process.env) { require('dotenv').config(); }
 
 export const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
