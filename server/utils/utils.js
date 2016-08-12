014845eea23dc235ef69d948b281ed332617aa8a
Keith Wong <keith.wong3672@gmail.com>
2016-08-12 09:10:55 -0700

    Create function to parse FoodFacts data

--- server/utils/utils.js
+++ server/utils/utils.js
@@ -1,30 +1,52 @@
 const request = require('request');
 // require('dotenv').config();
+const _ = require('lodash');
 
 const foodFactsHandler = (req, res) => {
   console.log('inside foodfacts api');
   const upc = req.body.event.data.slice(1);
-
+  console.log(upc);
   request.post(
     'https://api.foodfacts.com/ci/api/foodfacts/food_find_product_by_upc/format/json',
     {
       form: {
         login: process.env.FOOD_FACTS_LOGIN,
         password: process.env.FOOD_FACTS_PASSWORD,
-        upc: upc
+        upc: '014100085461'
+        // upc: upc
       }
     },
     function (error, response, body) {
       if (!error) {
         var parsedBody = JSON.parse(body);
-        console.log(parsedBody);
-        res.send(parsedBody.results);
+        var result = parseFoodFactsData(parsedBody.results);
+        res.send(result);
       } else {
         console.log('Food Facts API error', error);
       }
     }
   );
 };
+
+const parseFoodFactsData = (data) => {
+  console.log(data);
+  const allergies = {};
+  const diet = {};
+  const ingredientList = [];
+
+  const foodFactsData = {
+    allergies: '',
+    diet: '',
+    imgURL: '',
+    ingredientList: '',
+    reportCard: '',
+    score: '',
+  };
+  console.log(foodFactsData);
+  return foodFactsData;
+
+};
+
 
 const facebookHandler = (req, res) => {
   console.log('here in facebookHandler: ', req.body);
