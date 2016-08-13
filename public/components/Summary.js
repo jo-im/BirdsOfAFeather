f222ad8c8dae55329434e0f82ff4af2102bca538
Joann Im <joann_im@hotmail.com>
2016-08-13 11:39:26 -0700

    Delete display of user's concerns, allergies, and diets

--- public/components/Summary.js
+++ public/components/Summary.js
@@ -63,10 +63,6 @@ export default class Summary extends Component {
 
     return (
       <View>
-        <Text>{'\n'}</Text>
-        <Text>User's concerns are {this.props.concerns}</Text>
-        <Text>User's allergies are {this.props.allergies}</Text>
-        <Text>User's diets are {this.props.diets}</Text>
         <View style={{flex: 1, flexDirection: 'row'}}>
           <Image source={{uri: this.props.productImage }} style={{height: 150, width: 150}}></Image>
           <Text style={{textAlign: 'center', fontSize: 40, fontFamily: 'Didot-Italic'}}>Grade: {this.props.grade}</Text>
