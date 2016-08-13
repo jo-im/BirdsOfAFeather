ba89eec6bc72cd958a1f36ca7a6a19ec70f929a6
Keith Wong <keith.wong3672@gmail.com>
2016-08-12 18:13:44 -0700

    Enable local .env file to be used

--- server/db/dbConnection.js
+++ server/db/dbConnection.js
@@ -1,5 +1,5 @@
 import Sequelize from 'sequelize';
-// if (!process.env) { require('dotenv').config(); }
+if (!process.env) { require('dotenv').config(); }
 
 export const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
