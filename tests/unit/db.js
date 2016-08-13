54088c24770f9d2c75573001dbaa8d83d647bf6c
Keith Wong <keith.wong3672@gmail.com>
2016-08-12 18:12:28 -0700

    Created files for integration and unit testing

---
+++ tests/unit/db.js
@@ -0,0 +1,13 @@
+
+var expect = require('chai').expect;
+var Users = require('../../server-dist/db/models/userModel').Users;
+
+describe('Postgres unit tests', function() {
+
+  it('should retrieves all users', function(done) {
+    User.findAll().then(function(users) {
+      expect(users).to.be.an('array');
+      done();
+    }); 
+  });
+});
