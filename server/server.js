20793b4abbf15165dac78f61188dbc83305f25a9
Keith Wong <keith.wong3672@gmail.com>
2016-08-16 12:04:05 -0700

    Add Food Facts routes for text search

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const foodFactsHandler = require('./utils/utils.js').foodFactsHandler;
 const facebookHandler = require('./utils/utils.js').facebookHandler;
+const foodFactsSearchHandler = require('./utils/utils.js').foodFactsSearchHandler;
+
 const port = process.env.PORT || 3000;
 
 const app = express();
@@ -10,8 +12,9 @@ app.use(express.static(__dirname + '/../client/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/api/foodfacts/upc', foodFactsHandler);
 app.post('/api/facebook', facebookHandler);
+app.post('/api/foodfacts/search', foodFactsSearchHandler);
+app.post('/api/foodfacts/upc', foodFactsHandler);
 
 app.listen(port, function() {
   console.log('NodeJS Running on port', port);
