ceee0cbab7ca589bcb82a41ad1e42e622085ba11
Ryan McChesney <rmcchesn@gmail.com>
2016-08-17 16:54:42 -0700

    Add friends join model

---
+++ server/db/models/usersFriendsModel.js
@@ -0,0 +1,17 @@
+import {sequelize} from '../dbConnection';
+import Sequelize from 'sequelize';
+import {Users} from './userModel';
+
+// Join model -  users and associated friends.
+const Users_Friends = sequelize.define ('Users_Friends', {
+
+});
+
+Users.hasMany(Users);
+Users.belongsToMany(Users, {
+  as: 'friends',
+  through: 'Users_Friends',
+  foreignKey: 'userId'
+});
+
+export default Users_Friends;
