b279572d0c4eeebef0c9317e0933bbef43e09ce5
Joann Im <joann_im@hotmail.com>
2016-08-19 11:35:24 -0700

    Fix merge conflicts

--- public/components/Welcome.js
+++ public/components/Welcome.js
@@ -9,15 +9,18 @@ export default class Welcome extends Component {
         <Text>{"\n"}</Text>
         <Text style={style.styles.welcome}>Welcome {this.props.username}</Text>
         <Text style={style.styles.concernQuestion}>What are your concerns?</Text>
-        <TouchableHighlight style={style.styles.health} onPress={this.props.onSelectConcern.bind(this, 'Health')}>
-          <Text style={style.styles.text}>Health</Text>
-        </TouchableHighlight>
-        <TouchableHighlight style={style.styles.environment} onPress={this.props.onSelectConcern.bind(this, 'Environment')}>
-          <Text style={style.styles.text}>Environment</Text>
-        </TouchableHighlight>
-        <TouchableHighlight style={style.styles.society} onPress={this.props.onSelectConcern.bind(this, 'Society')}>
-          <Text style={style.styles.text}>Society</Text>
-        </TouchableHighlight>
+        <View style={{flex: 2, flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+          <TouchableHighlight style={style.styles.health} onPress={this.props.onSelectConcern.bind(this, 'Health')}>
+            <Text style={style.styles.text}>Health</Text>
+          </TouchableHighlight>
+          <TouchableHighlight style={style.styles.environment} onPress={this.props.onSelectConcern.bind(this, 'Environment')}>
+            <Text style={style.styles.text}>Environment</Text>
+          </TouchableHighlight>
+          <TouchableHighlight style={style.styles.society} onPress={this.props.onSelectConcern.bind(this, 'Society')}>
+            <Text style={style.styles.text}>Society</Text>
+          </TouchableHighlight>
+        </View>
+        <View style={style.styles.container}>
         <TouchableHighlight style={style.styles.next} onPress={this.props.onForward}>
           <Text style={style.styles.text}>Finish selecting concerns</Text>
         </TouchableHighlight>
@@ -27,6 +30,7 @@ export default class Welcome extends Component {
         <TouchableHighlight style={style.styles.next} onPress={this.props.goToProfile}>
           <Text style={style.styles.text}>Go To Profile</Text>
         </TouchableHighlight>
+        </View>
       </View>
     );
   }
