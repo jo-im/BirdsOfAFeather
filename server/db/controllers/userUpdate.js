c1a632ce2c75f14c137ee0648e4a5fa45229ae2e
Ryan McChesney <rmcchesn@gmail.com>
2016-08-19 16:04:25 -0700

    Implement updating of user data

---
+++ server/db/controllers/userUpdate.js
@@ -0,0 +1,27 @@
+import {sequelize} from '../dbConnection';
+import Sequelize from 'sequelize';
+import {Users} from '../models/userModel';
+
+export const setNewUser = (user) => {
+  let selectedUser = JSON.parse(user.data);
+
+  console.log('Trying to update user');
+
+  Users.update(
+    {
+      username: selectedUser.username,
+      profilePictUrl: selectedUser.url,
+      concerns: selectedUser.concerns,
+      email: selectedUser.email
+    },
+    { where: { facebookId: selectedUser.id } }
+  )
+  .then((updatedUser) => {
+    console.log('Success in updating user');
+    return updatedUser;
+  })
+  .catch((err) => {
+    console.log('Error in setting user ===> ', err);
+    return err;
+  });
+};
