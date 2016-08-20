012a9bc8cbecf73c95d4b7132212a6046448dd51
Joann Im <joann_im@hotmail.com>
2016-08-20 11:26:37 -0700

    Render the crab icon when you go back to the Allergies/Diet page

--- public/components/Allergies-Diet.js
+++ public/components/Allergies-Diet.js
@@ -19,7 +19,7 @@ export default class AllergiesAndDiet extends Component {
         <Text style={{textAlign: 'center', fontFamily: 'Didot-Italic', fontSize: 40}}>Any Allergies?</Text>
         <View style={{flex: 1, flexDirection: 'row'}}>
           <TouchableHighlight style={style.styles.allergyAndDietIcon} onPress={this.props.onSelectAllergy.bind(this, 'Crab')}>
-            <Image style={selectedStyle.iconDecoration} source={{uri: 'https://cdn0.iconfinder.com/data/icons/food-icons-rounded/110/Crab-128.png'}}></Image>
+            <Image style={style.styles.allergyAndDietIcon} source={{uri: 'https://cdn0.iconfinder.com/data/icons/food-icons-rounded/110/Crab-128.png'}}></Image>
           </TouchableHighlight>
           <TouchableHighlight style={style.styles.allergyAndDietIcon} onPress={this.props.onSelectAllergy.bind(this, 'Peanuts')}>
             <Image style={style.styles.allergyAndDietIcon} source={{uri: 'https://0.s3.envato.com/files/117161280/Allergen%20Icon%20Package%202/allergen_icons_500px_0001_peanuts.png'}}></Image>
