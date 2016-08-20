d570ec1611d9e63122c806c04992d25eb0b6a2ef
Keith Wong <keith.wong3672@gmail.com>
2016-08-20 16:33:10 -0700

    Change from local server to heroku server

--- public/components/TextSearch.js
+++ public/components/TextSearch.js
@@ -26,8 +26,8 @@ export default class TextSearch extends Component {
     console.log('inside searchProduct');
     console.log(this.state.searchTerm);
 
-    // fetch('https://murmuring-dusk-10598.herokuapp.com/api/foodfacts/upc', 
-    fetch('http://10.6.24.1:3000/api/foodfacts/search',
+    fetch('https://murmuring-dusk-10598.herokuapp.com/api/foodfacts/upc', 
+    // fetch('http://10.6.24.1:3000/api/foodfacts/search',
       {
         method: 'POST',
         headers:
