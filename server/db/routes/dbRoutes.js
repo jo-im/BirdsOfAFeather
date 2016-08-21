250a9618ab5c145b8a4915a9a82bf9a36235549a
Ryan McChesney <rmcchesn@gmail.com>
2016-08-21 16:02:14 -0700

    Include routes and handlers for each for DB related

--- server/db/routes/dbRoutes.js
+++ server/db/routes/dbRoutes.js
@@ -2,18 +2,16 @@ import {app} from '../../server';
 import {dbHandlers} from '../routeHandler/dbHandlers';
 
 ////////// FOLLOWING ENDPOINTS //////////
-app.post('/api/follows/add', () => {
-  console.log('hi');
-});
-// app.post('/api/follows/remove', dbHandlers.);
-// app.get('/api/follows/allForUser', dbHandlers.);
+app.post('/api/follows/add', dbHandlers.addNewFollowing);
+app.post('/api/follows/remove', dbHandlers.removeFollowing);
+app.get('/api/follows/allForUser', dbHandlers.getAllFollows);
 
 ////////// PRODUCT ENDPOINTS //////////
-// app.post('/api/', dbHandlers.);
-// app.post('/api/', dbHandlers.);
-// app.get('/api/', dbHandlers.);
+app.post('/api/product/addNew', dbHandlers.addNewProduct);
+app.post('/api/product/addComm', dbHandlers.addProductComment);
+app.get('/api/product/allForUser', dbHandlers.getAllUsersProducts);
 
 ////////// USER ENDPOINTS //////////
-// app.post('/api/user/new', dbHandlers.);
-// app.post('/api/user/update', dbHandlers.);
-// app.get('/api/user/confirm', dbHandlers.);
+app.post('/api/user/new', dbHandlers.addNewUser);
+app.post('/api/user/update', dbHandlers.updateUser);
+app.get('/api/user/confirm', dbHandlers.confirmUser);
