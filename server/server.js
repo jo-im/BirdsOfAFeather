db2b36c307de59b431163612df53ae929270d95a
Ryan McChesney <rmcchesn@gmail.com>
2016-08-22 10:35:18 -0700

    Import dbroutes to server

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ const facebookHandler = require('./utils/utils.js').facebookHandler;
 const foodFactsSearchHandler = require('./utils/utils.js').foodFactsSearchHandler;
 const foodFactsUPCHandler = require('./utils/utils.js').foodFactsUPCHandler;
 
+const friendsHandler = require('./utils/utils.js').friendsHandler;
+const userUpdateHandler = require('./utils/utils.js').userUpdateHandler;
+import dbRoutes from './db/routes/dbRoutes';
 
 const port = process.env.PORT || 3000;
 
@@ -16,6 +19,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.post('/api/facebook', facebookHandler);
 app.post('/api/foodfacts/search', foodFactsSearchHandler);
 app.post('/api/foodfacts/upc', foodFactsUPCHandler);
+
+// Routes for database interaction
+dbRoutes(app);
 
 app.listen(port, function() {
   console.log('NodeJS Running on port', port);
