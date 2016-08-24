f5362489802419d1dbcb5b0f54e3c86eaea1ec35
Keith Wong <keith.wong3672@gmail.com>
2016-08-23 17:38:35 -0700

    Change tests to run on Heroku

--- tests/unit/server.js
+++ tests/unit/server.js
@@ -8,14 +8,14 @@ var request = require('request');
 describe('Server Unit Tests', function() {
 
   it('should respond to GET request to server with a 200 status code', (done) => {
-    request('http://localhost:3000', (err, res, body) => {
+    request('https://murmuring-dusk-10598.herokuapp.com/', (err, res, body) => {
       expect(res.statusCode).to.equal(200);
       done();
     });
   });
 
   it('should respond to GET request with unknown endpoint with a 404 status code', (done) => {
-    request('http://localhost:3000/unknown', (err, res, body) => {
+    request('https://murmuring-dusk-10598.herokuapp.com/unknown', (err, res, body) => {
       expect(res.statusCode).to.equal(404);
       done();
     });
