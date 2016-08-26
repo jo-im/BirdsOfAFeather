4dd5aa312bce0c1bdcb6613384e711f31f6110af
Keith Wong <keith.wong3672@gmail.com>
2016-08-25 21:51:31 -0700

    Style search summary page

--- public/components/SearchResult.js
+++ public/components/SearchResult.js
@@ -1,5 +1,3 @@
-
-
 import React, { Component } from 'react';
 import { Text, TouchableHighlight, ListView, Image, View } from 'react-native';
 const _ = require('lodash');
@@ -17,38 +15,35 @@ export default class SearchResult extends Component {
   render() {
     _this = this;
     return (
-      <View>
-        <ListView
-          dataSource={this.state.dataSource}
-          renderRow={(rowData) => {
-            return (
-              <TouchableHighlight onPress={() => { 
-                _this.props.fetchItemUPC(rowData.product_upc);
-              }} key={rowData.product_upc}>
-                  <View>
-                    <Image source={{uri: rowData.product_image }} style={{height: 100, width: 100}}></Image>
-                    <Text> {rowData.title} </Text>
-                  </View>
-              </TouchableHighlight>
-            );
-          }}
-        />
-        <Text>{'\n'}</Text>
-        <View style={style.styles.container}>
+      <View style={{flex: 100, flexDirection: 'column'}}>
+        <View style={{flex: 5, marginTop: 20, marginBottom: 30, marginLeft: 350}}>
+          <TouchableHighlight onPress={this.props.goToProfile}>
+            <Image style={{height: 50, width: 50}} source={{uri: 'http://cornerstonecoastal.com/wp-content/plugins/bright/images/menu-icon.png'}}></Image>
+          </TouchableHighlight>
+        </View>
+        <View style={{flex: 85}}>
+          <ListView
+            dataSource={this.state.dataSource}
+            renderRow={(rowData) => {
+              return (
+                <TouchableHighlight onPress={() => { 
+                  _this.props.fetchItemUPC(rowData.product_upc);
+                }} key={rowData.product_upc}>
+                    <View style={{flex: 100, flexDirection: 'row', alignItems: 'center', borderTopWidth: 2, padding: 5}}>
+                      <Image source={{uri: rowData.product_image }} style={{flex: 15, height: 80}}></Image>
+                      <Text style={{flex: 85, fontSize: 20, fontFamily: 'Georgia-Bold', marginLeft: 15}}> {rowData.title} </Text>
+                    </View>
+                </TouchableHighlight>
+              );
+            }}
+          />
+          </View>
+        <View style={{flex: 10, alignItems: 'center', justifyContent: 'center'}}>
           <TouchableHighlight style={style.styles.back} onPress={this.props.onBack}>
-            <Text style={style.styles.text}>Go Back</Text>
+            <Text style={style.styles.navigationText}>Back</Text>
           </TouchableHighlight>
         </View>
       </View>
     );
   }
-
 }
-
-
-        // <View style={{flex: 1, flexDirection: 'row'}}>
-        //   <Image source={{uri: this.props.searchResult[0].product_image }} style={{height: 150, width: 150}}></Image>
-        // </View>
-        // <Text>{'\n'}</Text>
-        // <Text>{'\n'}</Text>
-        // <Text>{'\n'}</Text>
